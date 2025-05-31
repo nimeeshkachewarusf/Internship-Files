406e279ef638f80fbfa36aafb201b187150e0719
NIMEESH KACHEWAR <156560079+nimeeshkachewarusf@users.noreply.github.com>
2025-05-31 16:03:45 +0530

    Update synthetic_data_python.py
    Location data script modified

--- synthetic_data_python.py
+++ synthetic_data_python.py
@@ -61,7 +61,6 @@ for i in range(1,25000):
 
     raw_time = f.date_time()
     year_range = f.date_between(start_date='-4y',end_date='today')
-    new_time = raw_time.strftime(f'{raw_time.month}/{raw_time.day}/{year_range.year} {raw_time.hour}:{raw_time.minute}')
     
     latitude_initial = float(f.latitude())
     longitude_initial = float(f.longitude())
@@ -72,10 +71,22 @@ for i in range(1,25000):
         random2 = float(random.uniform(-0.0008,0.0065))
         latitude_initial += random1
         longitude_initial += random2
+        increment = pan.Timestamp(raw_time) + pan.Timedelta(minutes = j)
+
+        try:
+
+            change_year = increment.replace(year = year_range.year)
+            
+        except:
+            change_year = increment.replace(year = year_range.year, month = year_range.month, day = 10)
+        
+        time_to_display = change_year.strftime(f'{change_year.month}/{change_year.day}/{change_year.year} {change_year.hour}:{change_year.minute}')
+
+       
 
 
         collection3 = {'Id' : con,
-                    'DateTime' : new_time,
+                    'DateTime' : time_to_display,
                     'DeviceId': f'Device{i}',
                     'Latitute' : f'{latitude_initial:.6f}',
                     'Longitude' : f'{longitude_initial:.6f}'}
@@ -84,7 +95,7 @@ for i in range(1,25000):
         location_data.append(collection3)
 
 data_frame3 = pan.DataFrame(location_data)
-data_frame3.to_excel('location_data_final.ods', engine = 'odf', index=False)
+data_frame3.to_excel('location_data_100.ods', engine = 'odf', index=False)
 
 # --------- Location Data Script Ends -----------------------------------------
 
